39a2fcab3c11fb5e37f9b2eb535ba6fec414219c
Prince <Prince@scriptbytes.in>
2026-07-29 11:44:22 +0530

    server & folder added for config

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-console.log('Hello BPS')
+const express = require('express')
+const app = express();
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 3001
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.url}`)
+    next()
+})
+
+app.get('/api/home',(req,res)=>{
+    res.send("Welcome To Bottle Production Suite (BPS)")
+})
+
+app.listen(port, ()=>{
+    console.log(`Port Listening to ${port}`)
+})
