889116a9d72113b65b2b6d4193ac48d274f32104
Prince <Prince@scriptbytes.in>
2026-08-12 16:47:37 +0530

    frequency number

--- JS/Hashing que.js
+++ JS/Hashing que.js
@@ -1,3 +1,4 @@
+//use fetch or axios for calling from thrid party api question need to solve EG
 //1. Count frequency of each element using hashing
 /* Brute Force
 let hashNum = [10,1,2,10,2,1,15];
@@ -23,6 +24,8 @@ for(let i of hashNum){
 }
 console.log(hashKey);
 */
+
+/* 2. Find Duplicate using Hashing
 let hashNum = [10,1,2,10,2,1,15];
 let fetchDetails={}
 
@@ -34,3 +37,30 @@ for(let i of hashNum){
     }
 }
 console.log(fetchDetails);
+*/
+
+/* 3. Find the highest and lowest frequency element. how many which number a appered maximum
+let hashNum = [10,2,10,2,10,15];
+let result ={};
+let max = -Infinity;
+let min = Infinity;
+let maximum;
+let minimum;
+for(let i of hashNum){
+    result[i]=(result[i] || 0) + 1
+}
+
+for(let i in result){
+    if(result[i]>max){
+        max=result[i];
+        maximum=i
+    }
+    if(result[i]<min){
+        min=result[i];
+        minimum=i
+    }
+}
+
+console.log('maximum ' + maximum);
+console.log('minimum ' + minimum);
+*/
