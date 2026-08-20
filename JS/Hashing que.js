6d9f9cb949d88cc09d27394bae629858f41edae6
Prince <Prince@scriptbytes.in>
2026-08-20 15:23:30 +0530

    some leetcode questions

--- JS/Hashing que.js
+++ JS/Hashing que.js
@@ -64,3 +64,102 @@ for(let i in result){
 console.log('maximum ' + maximum);
 console.log('minimum ' + minimum);
 */
+
+/* 4. Palidrom
+let value = 'RACECAR';
+let check = value.toLowerCase();
+let rev = check.split("").reverse().join("");
+
+if(rev != check){
+    console.log('Not An Palnidrom')
+}else{
+    console.log('is An Palnidrom')
+}
+*/
+/*
+let pairsHaspMap = { "(": ")", "{": "}", "[": "]" };
+function stacks(values) {
+    let stack = [];
+    let isValid = true;
+    for (let i of values) {
+        let char = i;
+        if (pairsHaspMap[char]) {
+            stack.push(char)
+        } else if (pairsHaspMap[stack.pop()] != char) {
+            isValid = false;
+            console.log('Value ' + char);
+            break
+        }
+    }
+    if (stack.length != 0) {
+        isValid = false
+    }
+    return isValid
+}
+
+console.log(stacks("()[{}()]")); // true
+console.log(stacks("([)]"));     // false
+console.log(stacks("(((")); 
+*/
+
+/* Remove duplicate number
+//1.
+let numbers = [1,2,3,4,5,6,7,9,2];
+let results= [...new Set(numbers)]
+console.log(results);
+
+//1.1
+let numbers = [1,2,3,4,5,6,7,9,2];
+let result=[];
+
+for(let i of numbers){
+    if(!result.includes(i)){
+        result.push(i)
+    }
+}
+console.log(result);
+
+//2. Check has duplicate
+let numbers = [1,2,3,4,5,6,7,9,2];
+let results= new Set();
+for(let i of numbers){
+    if(results.has(i)){
+        console.log('i ' + i)
+        break;
+    }
+    results.add(i);
+}
+
+//2.2
+
+let hashNum = [10,1,2,10,2,1,15];
+let fetchDetails={}
+
+for(let i of hashNum){
+    fetchDetails[i] = (fetchDetails[i] || 0) + 1
+    if(fetchDetails[i]>1){
+        console.log(i);
+        break;
+    }
+}
+console.log(fetchDetails);
+
+//2.3
+let numbers = [1,2,3,4,5,6,7,9,2];
+let result={};
+let hasDuplicate = false;
+
+for(let i of numbers){
+    if(result[i]){
+        console.log(i)
+        hasDuplicate=true;
+        break
+    }else{
+        result[i]=true
+    }
+}
+console.log(hasDuplicate);
+
+
+
+*/
